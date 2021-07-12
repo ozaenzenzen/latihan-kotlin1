44af8572570f6fe46fc46f184addff168bd3b85e
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-07-12 14:42:57 +0700

    learn android lifecycle

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -4,7 +4,9 @@ import android.app.KeyguardManager
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
 import java.security.Key
 
 class MainActivity : AppCompatActivity() {
@@ -12,13 +14,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textview: TextView = findViewById(R.id.textView)
-        //type casting - as
-        val keyMgr =  getSystemService(Context.KEYGUARD_SERVICE) as? KeyguardManager
-        if(keyMgr is KeyguardManager){
-            //do something
-        }
-
-//        textview.setText(result.toString())
+        Toast.makeText(this, "OnCreate function", Toast.LENGTH_LONG)
+        Log.i("info", "onCreate Function Executed!")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "OnStart function", Toast.LENGTH_LONG)
+        Log.i("info", "OnStart Function Executed!")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "OnResume function", Toast.LENGTH_LONG)
+        Log.i("info", "OnResume Function Executed!")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "onPause function", Toast.LENGTH_LONG)
+        Log.i("info", "onPause Function Executed!")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "onStop function", Toast.LENGTH_LONG)
+        Log.i("info", "onStop Function Executed!")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "onDestroy function", Toast.LENGTH_LONG)
+        Log.i("info", "onDestroy Function Executed!")
+    }
+
+
 }
